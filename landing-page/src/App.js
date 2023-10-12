3824bd9246bcab6ed0a225ffcd0caa9dd5b46876
Lydia Numwali <numwalilydia2022@gmail.com>
2023-10-12 16:59:57 +0200

    Initial commit

---
+++ landing-page/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div className='container'>
+        <h2 id='logo'>Startup 3</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
